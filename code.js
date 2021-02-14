fa988b7e870e2b11d4d9dec1ead4231a37f73fe5
aaarland <adrian.aarland@gmail.com>
2021-02-13 18:45:40 -0600

    first draft of homepage

---
+++ code.js
@@ -0,0 +1,34 @@
+function generateHTML(data){
+    var projects = document.getElementById("projects");
+
+    for(var i = 0; i < data.length; i++){
+        let elem = document.createElement("li")
+
+        var title = document.createElement("h2")
+        var desc = document.createElement("p")
+        var link = document.createElement("a")
+
+        console.log(data[i])
+
+        title.innerHTML = data[i].title
+        desc.innerHTML = data[i].description
+        link.innerHTML = data[i].github
+        link.setAttribute("href", data[i].github)
+        elem.appendChild(title)
+        elem.appendChild(desc)
+        elem.appendChild(link)
+
+        projects.appendChild(elem)
+
+    }
+    //document.getElementById("projects").appendChild(elements)
+
+}
+function printData(data){
+    for(var i = 0; i < data.length; i++){
+        console.log(data[i]);
+    }
+}
+fetch("projects.json")
+    .then(res => res.json())
+    .then(data => generateHTML(data))
