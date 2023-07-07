5bed7b273055ef63cb1dec6a0620351c466eac34
Adrian Aarland <60984716+aaarland@users.noreply.github.com>
2023-07-06 21:04:41 -0500

    I wish typescript worked in the web

--- name-stripper.js
+++ name-stripper.js
@@ -1,10 +1,13 @@
 function transform() {
     const text = document.getElementById("names");
+    /**
+     * @type string[]
+     */
     const lines = text.value.split("\n");
     const transformedlines = lines. map((line) => {
         const words = line.split(' ');
         words[0] = words[0].substring(0, 1);
         return words[0] + ' ' + words[1];
     });
-    text.value = transformedlines.doin("\n");
+    text.value = transformedlines.join("\n");
 }
