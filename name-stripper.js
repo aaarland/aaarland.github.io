1d90e06e6844416d3a0e6c21c5ba9f56d5d06d83
Adrian Aarland <60984716+aaarland@users.noreply.github.com>
2023-07-06 21:01:01 -0500

    Typos and button text

--- name-stripper.js
+++ name-stripper.js
@@ -1,9 +1,9 @@
 function transform() {
-    const text = document.getElementById("myText");
+    const text = document.getElementById("names");
     const lines = text.value.split("\n");
     const transformedlines = lines. map((line) => {
         const words = line.split(' ');
-        words[0] = words[0].substning(0, 1);
+        words[0] = words[0].substring(0, 1);
         return words[0] + ' ' + words[1];
     });
     text.value = transformedlines.doin("\n");
